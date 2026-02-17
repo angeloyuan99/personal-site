98b623289fc6f42f32059d07ca922e807eec5c83
angeloyuan99 <106442240+angeloyuan99@users.noreply.github.com>
2026-02-16 22:21:51 -0500

    updated experience

--- components/Portfolio.tsx
+++ components/Portfolio.tsx
@@ -32,6 +32,12 @@ const Portfolio: React.FC = () => {
       brand: "Personal Brand",
       description: "Grew a personal Instagram to 80K+ followers in 2 months, generating 20M+ views.",
       link: "https://www.instagram.com/itz_angelo2006/"
+    },
+    {
+      period: "2025 AUG - DEC",
+      brand: "DeepRoyale",
+      description: "Built a Clash Royale notetaking and match analysis web app that reached 2000+ active users.",
+      link: "https://www.deeproyale.com/"
     }
   ];
 
